5a444158fc5f9e31540d85df0e9bfeebdf8a04be
gaoys <923913419@qq.com>
2024-02-12 21:51:50 +0800

    first commit

---
+++ djangoProject/app/views.py
@@ -0,0 +1,96 @@
+import json
+
+from django.shortcuts import render
+from django.http import JsonResponse
+from elasticsearch_dsl import *
+from elasticsearch_dsl.query import *
+
+connections.create_connection(hosts=['localhost'])
+
+def search_query(search,query,columns):
+    s=search.query(query)
+    response=s.execute()
+
+    for hit in response.hits:
+        for col in columns:
+            print(f'{col} : {hit.to_dict().get(col)}')
+
+
+
+def law_query(request):
+    if request.method == 'POST':
+        law_number = request.POST.get('law_number', '')
+        law_content = request.POST.get('law_content','')
+        fact_content = request.POST.get('fact_content','')
+        search = Search(index='law')
+        query = Match(num=law_number)
+        if law_content:
+            search = Search(index='law')
+            query= Match(content=law_content)
+        if law_number:
+            search = Search(index='law')
+            query = Match(num=law_number)
+        if fact_content:
+            search = Search(index='fact')
+            query=Match(fact=fact_content)
+        response = search.query(query).execute()
+        results=response.hits
+        print(results)
+        return render(request, 'law_result.html',{'results':results})
+    return render(request, 'law_query.html')
+
+def getLawByNum(request):
+    if request.method=="POST":
+        request_data = json.loads(request.body)
+        lawNum = request_data.get("lawNum")
+        search = Search(index='law')
+        lawList=[]
+        if lawNum:
+            for i in range(int(lawNum), int(lawNum) + 10):
+                query = Match(num=i)
+                response = search.query(query).execute()
+                results = response.hits
+                print(results)
+                tmp={'num':results[0].num,'content':results[0].content}
+                lawList.append(tmp)
+            response = JsonResponse({'law':json.dumps(lawList)})
+            response['Access-Control-Allow-Origin'] = '*'
+            return response
+        else:
+            response = JsonResponse({'error':'错误'})
+            response['Access-Control-Allow-Origin'] = '*'
+            return response
+
+def getLawByContent(request):
+    if request.method=="POST":
+        request_data = json.loads(request.body)
+        lawContent = request_data.get("lawContent")
+        search = Search(index='law')
+        lawList=[]
+        if lawContent:
+            query = Match(content=lawContent)
+            response = search.query(query).execute()
+            results = response.hits
+            if results:
+                for result in results:
+                    tmp = {'num': result.num, 'content': result.content}
+                    lawList.append(tmp)
+                response = JsonResponse({'law': json.dumps(lawList)})
+                response['Access-Control-Allow-Origin'] = '*'
+                return response
+            else:
+                response = JsonResponse({'error': "未找到相关法条"})
+                response['Access-Control-Allow-Origin'] = '*'
+                return response
+        else:
+            response = JsonResponse({'error':'错误'})
+            response['Access-Control-Allow-Origin'] = '*'
+            return response
+# def getCaseByKeyWords(request):
+#     if request.method=='POST':
+#         request_data = json.loads(request.body)
+#         factContent = request_data.get("fact_content")
+#         search = Search(index='law')
+
+
+
---
+++ vueProject/src/components/CaseSearch.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <div class="search-box">
+      <input
+        type="text"
+        placeholder="在百度中搜索"
+        v-model="searchQuery"
+        @keyup.enter="search"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  methods: {
+    search() {
+      // 在这里执行搜索操作，可以使用 this.searchQuery 来获取搜索关键词
+      console.log("搜索：", this.searchQuery);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+    justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
+  height: 100vh; /* 使容器铺满整个视口高度 */
+}
+.search-box {
+  width: 800px; /* 设定搜索框的宽度 */
+  margin: 0 auto; /* 水平居中 */
+  padding: 20px;
+  background-color: #f2f2f2;
+  border-radius: 30px; /* 圆角 */
+}
+
+.search-box input[type="text"] {
+  width: 100%; /* 搜索框铺满整个容器 */
+  padding: 10px;
+  font-size: 16px;
+  border: none;
+  border-radius: 30px; /* 圆角 */
+  outline: none;
+}
+</style>
